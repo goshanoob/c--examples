18d9ab39bda86b670eb8bd17c3fcc47ed907e511
goshanoob <goshanoob@mail.ru>
2022-01-18 19:55:46 +0500

    Добавлена задача 18

--- BasicCpp/z1.cpp
+++ BasicCpp/z1.cpp
@@ -15,10 +15,10 @@ void z1() {
 	cout << x1 + x2 << endl;
 }
 
-//int z2();
+int z2();
 int z3();
-//int z4();
-//int z5();
+int z4();
+int z5();
 int z6();
 int z7();
 int z8();
@@ -28,10 +28,10 @@ int main()
 {
    	setlocale(LC_ALL, "");
 	z1();
-	//z2();
+	z2();
 	z3();
-	//z4();
-	//z5();
+	z4();
+	z5();
 	z6();
 	z7();
 	z8();
